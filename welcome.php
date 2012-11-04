ecddf230bd7f6fd4a2f4829fae65e1ca571ea66e
Barbara Wolff Dick <barbara@quavio.com.br>
2012-11-04 16:01:53 -0200

    final text for welcome. darken layout for new theme.

--- welcome.php
+++ welcome.php
@@ -1,26 +1,34 @@
 <div id="welcome">
-  <div class="row">
-    <div class="offset1 span7">
-      <h2>Welcome</h2>
-      <p>Loose networks made possible by new technologies that may consist of both amateurs and professionals joined to solve a problem. The changing environment of the new economy, mobility of working environments, connectivity and social media has led to crowdsourcing being a 21st century phenomenon.
-        <br /><br />
-        Crowdsourcing Week Asia (CSWA) will bring together the best and brightest minds and innovators in the field today. A curated event that will highlight new directions in crowdsourcing</p>
-    </div>
-    <div class="offset1 span3">
-      <div id="open-registration">
-        <div class="pad">
-          <div class="inner">
-            <h2>
-              <span>Registration is</span>
-              <span class="open">Open</span>
+  <div class="container">
+    <div class="row">
+      <div class="span8">
+        <h2>The Future of Ideas, Innovation & Creativity</h2>
+        <h3>{Crowdsourcing}</h3>
+        <p>Loose networks made possible by new technologies that may consist of both amateurs and professionals joined to solve a problem.</p>
+        <br />
+        <h3>Changing Environments</h3>
+        <p>The changing environment of the new economy, mobility of working environments, connectivity and social media has led to crowdsourcing being a 21st century phenomenon.</p>
 
-            </h2>
-            <button class="btn btn-warning btn-save-the-date">Save the date</button>
+        <br />
+        <h3>Best & Brightest</h3>
+        <p>Crowdsourcing Week Asia (CSWA) will bring together the best and brightest minds and innovators in the field today. A curated event that will highlight new directions in crowdsourcing</p>
+        <br />
+      </div>
+      <div class="offset1 span3">
+        <div id="open-registration">
+          <div class="pad">
+            <div class="inner">
+              <h2>
+                <span>Registration is</span>
+                <span class="open">Open</span>
+
+              </h2>
+              <button class="btn btn-warning btn-save-the-date">Save the date</button>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-
   
 </div>
