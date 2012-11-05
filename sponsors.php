46612224be73e5fd5458584444886c51e24278af
Barbara Wolff Dick <barbara@quavio.com.br>
2012-11-05 13:01:08 -0200

    sponsors section is ok. more images should come later

--- sponsors.php
+++ sponsors.php
@@ -5,7 +5,7 @@
         <h2>Crowdsourcing Sponsors</h2>
         <ul class="sponsors-list">
           <li>
-            <a href='#'>
+            <a href='http://www.99designs.com'>
               <div class="sponsor-logo">
                 <img src="../wp-content/themes/csweek/images/99design.png">
               </div>
@@ -13,7 +13,7 @@
           </li>
 
           <li>
-            <a href='#'>
+            <a href='http://www.chaordix.com/'>
               <div class="sponsor-logo">
                 <img src="../wp-content/themes/csweek/images/chaordix.png">
               </div>
@@ -21,7 +21,7 @@
           </li>
 
           <li>
-            <a href='#'>
+            <a href='http://zooppa.com/'>
               <div class="sponsor-logo">
                 <img src="../wp-content/themes/csweek/images/zooppa.png">
               </div>
@@ -29,7 +29,7 @@
           </li>
 
           <li>
-            <a href='#'>
+            <a href='http://www.imagebrief.com/'>
               <div class="sponsor-logo">
                 <img src="../wp-content/themes/csweek/images/imagebrief.png">
               </div>
@@ -37,14 +37,14 @@
           </li>
 
           <li>
-            <a href='#'>
+            <a href='http://en.eyeka.com/'>
               <div class="sponsor-logo">
                 <img src="../wp-content/themes/csweek/images/eyeka.png">
               </div>
             </a>
           </li>
           <li>
-            <a href='#'>
+            <a href='http://crowdflower.com/'>
               <div class="sponsor-logo">
                 <img src="../wp-content/themes/csweek/images/crowdflower.png">
               </div>
@@ -52,7 +52,7 @@
           </li>
           
           <li>
-            <a href='#'>
+            <a href='http://www.growvc.com/main/'>
               <div class="sponsor-logo">
                 <img src="../wp-content/themes/csweek/images/growvc.png">
               </div>
@@ -60,7 +60,7 @@
           </li>
           
           <li>
-            <a href='#'>
+            <a href='http://www.razoo.com/'>
               <div class="sponsor-logo">
                 <img src="../wp-content/themes/csweek/images/razoo.png">
               </div>
@@ -74,19 +74,27 @@
       <h2>Technology partners</h2>
         <ul class="sponsors-list">
           <li>
-            <a href='#'>
+            <a href='http://www.yoose.com/'>
               <div class="sponsor-logo">
                 <img src="../wp-content/themes/csweek/images/yoose.png">
               </div>
             </a>
           </li>
           <li>
-            <a href='#'>
+            <a href='http://www.appbackr.com/'>
               <div class="sponsor-logo">
                 <img src="../wp-content/themes/csweek/images/appbackr.png">
               </div>
             </a>
           </li>
+           <li>
+            <a href='http://www.engage.is'>
+              <div class="sponsor-logo">
+                <img src="../wp-content/themes/csweek/images/engage.png">
+              </div>
+            </a>
+          </li>
+
 
         </ul>
     </div>
@@ -94,7 +102,7 @@
       <h2>Venue</h2>
         <ul class="sponsors-list">
           <li>
-            <a href='#'>
+            <a href='http://maxatria.com.sg/'>
               <div class="sponsor-logo">
                 <img src="../wp-content/themes/csweek/images/maxatria.png">
               </div>
