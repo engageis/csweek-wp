4104394d24c2d01fa25dff056d89480b8b2f49ab
Barbara Wolff Dick <barbara@quavio.com.br>
2012-11-05 11:37:36 -0200

    adding sponsors logos in home

--- sponsors.php
+++ sponsors.php
@@ -1,13 +1,13 @@
 <div id="sponsors">
   <div class="container">
     <div class="row">
-      <div class="span12">
+      <div class="span12"u>
         <h2>Crowdsourcing Sponsors</h2>
-        <ul>
+        <ul class="sponsors-list">
           <li>
             <a href='#'>
               <div class="sponsor-logo">
-                <img src=".../wp-contect/themes/csweek/images/99design.png">
+                <img src="../wp-content/themes/csweek/images/99design.png">
               </div>
             </a>
           </li>
@@ -15,7 +15,7 @@
           <li>
             <a href='#'>
               <div class="sponsor-logo">
-                <img src="../wp-contect/themes/csweek/images/eyeka.png">
+                <img src="../wp-content/themes/csweek/images/eyeka.png">
               </div>
             </a>
           </li>
@@ -23,7 +23,7 @@
           <li>
             <a href='#'>
               <div class="sponsor-logo">
-                <img src="../wp-contect/themes/csweek/images/zooppa.png">
+                <img src="../wp-content/themes/csweek/images/zooppa.png">
               </div>
             </a>
           </li>
@@ -31,12 +31,69 @@
           <li>
             <a href='#'>
               <div class="sponsor-logo">
-                <img src="../wp-contect/themes/csweek/images/growvc.png">
+                <img src="../wp-content/themes/csweek/images/growvc.png">
               </div>
             </a>
           </li>
-        <h2>Technology partners</h2>
 
-      </div>
+          <li>
+            <a href='#'>
+              <div class="sponsor-logo">
+                <img src="../wp-content/themes/csweek/images/razoo.png">
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <div class="sponsor-logo">
+                <img src="../wp-content/themes/csweek/images/crowdflower.png">
+              </div>
+            </a>
+          </li>
+          
+          <li>
+            <a href='#'>
+              <div class="sponsor-logo">
+                <img src="../wp-content/themes/csweek/images/imagebrief.png">
+              </div>
+            </a>
+          </li>
+          
+          <li>
+            <a href='#'>
+              <div class="sponsor-logo">
+                <img src="../wp-content/themes/csweek/images/chaordix.png">
+              </div>
+            </a>
+          </li>
+
+
+        </ul>
+    </div>  
+    <div class="span12">
+      <h2>Technology partners</h2>
+        <ul class="sponsors-list">
+          <li>
+            <a href='#'>
+              <div class="sponsor-logo">
+                <img src="../wp-content/themes/csweek/images/chaordix.png">
+              </div>
+            </a>
+          </li>
+        </ul>
     </div>
+    <div class="span12">
+      <h2>Venue</h2>
+        <ul class="sponsors-list">
+          <li>
+            <a href='#'>
+              <div class="sponsor-logo">
+                <img src="../wp-content/themes/csweek/images/chaordix.png">
+              </div>
+            </a>
+          </li>
+        </ul>
+
+    </div>
+  </div>
 </div>
