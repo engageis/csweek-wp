0ceabd55781a88d6e668fe616b44b9e06c55ff82
Barbara Wolff Dick <barbara@quavio.com.br>
2012-11-20 11:13:40 -0200

    adding home content

--- wp-content/themes/csweek/_footer.php
+++ wp-content/themes/csweek/_footer.php
@@ -1,7 +1,10 @@
 <footer id="footer">
-  <div class="row">
-    <div class="offset1 span11">
-      <h2>Contact Info</h2>
-      <p>Loose networks made possible by new technologies that may consist of both amateurs and professionals joined to solve a problem. The changing environment of the new economy, mobility of working environments, connectivity and social media has led to crowdsourcing being a 21st century phenomenon.</p>
+  <div class="container">
+    <div class="row">
+      <div class="span12">
+        <h2>Contact Info</h2>
+        <p>Loose networks made possible by new technologies that may consist of both amateurs and professionals joined to solve a problem. The changing environment of the new economy, mobility of working environments, connectivity and social media has led to crowdsourcing being a 21st century phenomenon.</p>
+      </div>
     </div>
+  </div>
 </div>
