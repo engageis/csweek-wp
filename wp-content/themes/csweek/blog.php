5e66e16e67affe6af6bab6099e280ccc43462ab9
Gustavo Guichard <gustavoguichard@gmail.com>
2012-12-04 17:12:46 -0200

    More testing

--- wp-content/themes/csweek/blog.php
+++ wp-content/themes/csweek/blog.php
@@ -21,7 +21,7 @@ function path($p){
     <ul>
 <?php
 // The Query
-echo "Hey";
+echo $query_string;
 $my_query = new WP_Query('showposts=3&post_type=post');
 if($my_query->have_posts()) : while($my_query->have_posts()) : $my_query->the_post();
   echo '<li>';
