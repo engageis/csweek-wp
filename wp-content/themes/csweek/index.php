ece6b2f7a4b986f4a003370c0202287685915fad
Gustavo Guichard <gustavoguichard@gmail.com>
2012-12-05 14:56:09 -0200

    Changes blog loop

--- wp-content/themes/csweek/index.php
+++ wp-content/themes/csweek/index.php
@@ -1,37 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <? require '_head.php' ?>
-  </head>
-
-  <body data-spy="scroll" data-target=".bs-docs-sidebar">
-    <? require '_navbar.php' ?>
-
-    <div>
-      <? require '_splash-sec.php'?> 
-      <? require '_menu.php' ?>
+<?php get_header(); ?>
+<?php if ( ! have_posts() ) : ?>
+  <article class="post e404">
+    <h2>Opa, ainda não há conteúdo aqui...</h2>
+    <p>Alguma explicação.</p>
+  </article>
+<?php endif; ?>
+<?php while ( have_posts() ) : the_post(); ?>
+  <article <?php post_class(); ?>>
+    <header>
+      <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+      <span class="category-show">
+        <?php the_time('l, j \d\e F \d\e Y');?>
+      </span>
+    </header>
+    <div class="entry-content">
+      <?php if(has_post_thumbnail($post->ID)):?><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail('big-thumb');?></a><?php endif;?>
+      <?php the_excerpt(); ?>
     </div>
+  </article>
+<?php endwhile; // End the loop. Whew. ?>
 
-
-    <ul>
-      <?php
-      // The Query
-      echo $query_string;
-      $my_query = new WP_Query('showposts=3&post_type=post');
-      if($my_query->have_posts()) : while($my_query->have_posts()) : $my_query->the_post();
-        echo '<li>';
-        the_title();
-        echo '</li>';
-      endwhile; endif;
-      // Reset Query
-      wp_reset_query();
-      ?>
-    </ul>
-
-    <? require '_sponsors.php'?> 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-    <script src="<?= path("bootstrap/js/bootstrap.js") ?>"></script>
-    <script src="<?= path("js/lines.js") ?>"></script>
-  </body>
-</html>
+<aside class="pagination">
+  <?php if(get_next_posts_link()):?>
+  <div id="prev_post">
+    <?php next_posts_link('Antigos');?>
+  </div>
+  <?php endif;?>
+  <?php if(get_previous_posts_link()):?>
+  <div id="next_post">
+    <?php previous_posts_link('Novos');?>
+  </div>
+  <?php endif;?>
+</aside>
+<?php get_footer(); ?>
