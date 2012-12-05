d6f8a181c9702f1a84c9ca980c62b29db142dd1f
Juan Maiz <juanmaiz@gmail.com>
2012-12-05 15:27:01 -0200

    formatando blog legal

--- wp-content/themes/csweek/index.php
+++ wp-content/themes/csweek/index.php
@@ -1,35 +1,46 @@
 <?php get_header(); ?>
-<?php if ( ! have_posts() ) : ?>
-  <article class="post e404">
-    <h2>Opa, ainda não há conteúdo aqui...</h2>
-    <p>Alguma explicação.</p>
-  </article>
-<?php endif; ?>
-<?php while ( have_posts() ) : the_post(); ?>
-  <article <?php post_class(); ?>>
-    <header>
-      <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-      <span class="category-show">
-        <?php the_time('l, j \d\e F \d\e Y');?>
-      </span>
-    </header>
-    <div class="entry-content">
-      <?php if(has_post_thumbnail($post->ID)):?><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail('big-thumb');?></a><?php endif;?>
-      <?php the_excerpt(); ?>
-    </div>
-  </article>
-<?php endwhile; // End the loop. Whew. ?>
+<style type="text/css">
+#page .entry-content p{
+  color: #ddd;
+}
+</style>
+<div id='page'>
+  <div class='container'>
 
-<aside class="pagination">
-  <?php if(get_next_posts_link()):?>
-  <div id="prev_post">
-    <?php next_posts_link('Antigos');?>
+    <?php if ( ! have_posts() ) : ?>
+      <article class="post e404">
+        <h2>Opa, ainda não há conteúdo aqui...</h2>
+        <p>Alguma explicação.</p>
+      </article>
+    <?php endif; ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+      <article style="margin-bottom:30px" <?php post_class(); ?>>
+        <header>
+          <h2 style="margin:0" class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+          <span style="color:#999" class="category-show">
+            <?php the_time('l, j \d\e F \d\e Y');?>
+          </span>
+        </header>
+        <div class="entry-content">
+          <?php if(has_post_thumbnail($post->ID)):?><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail('big-thumb');?></a><?php endif;?>
+          <?php the_excerpt(); ?>
+        </div>
+      </article>
+      <hr style="border-top: 1px solid rgba(0,0,0,0.2); border-bottom: 1px solid rgba(255,255,255,0.2)" />
+    <?php endwhile; // End the loop. Whew. ?>
+
+    <aside class="pagination">
+      <?php if(get_next_posts_link()):?>
+      <div id="prev_post">
+        <?php next_posts_link('Antigos');?>
+      </div>
+      <?php endif;?>
+      <?php if(get_previous_posts_link()):?>
+      <div id="next_post">
+        <?php previous_posts_link('Novos');?>
+      </div>
+      <?php endif;?>
+    </aside>
   </div>
-  <?php endif;?>
-  <?php if(get_previous_posts_link()):?>
-  <div id="next_post">
-    <?php previous_posts_link('Novos');?>
-  </div>
-  <?php endif;?>
-</aside>
+</div>
 <?php get_footer(); ?>
