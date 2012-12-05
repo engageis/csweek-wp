1de0298f6f42ccf21b388403bf5228a185f109ba
Gustavo Guichard <gustavoguichard@gmail.com>
2012-12-05 15:11:22 -0200

    Modifies page.php

--- wp-content/themes/csweek/page.php
+++ wp-content/themes/csweek/page.php
@@ -1,42 +1,31 @@
 <?php
-function path($p){
-  return "wp-content/themes/csweek/$p";
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <? require '_head.php' ?>
-  </head>
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the wordpress construct of pages
+ * and that other 'pages' on your wordpress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage CSWeek
+ * @since Maiz Lulkin 2013
+ */
 
-  <body data-spy="scroll" data-target=".bs-docs-sidebar">
-    <? require '_navbar.php' ?>
-
-    <div>
-      <? require '_splash-sec.php'?> 
-      <? require '_menu.php' ?>
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-      <div id='page'>
-        <div class='container'>
-          <article>
-            <header>
-              <?php $page_quoted_title = get_post_meta($post->ID, 'page_quoted_title', true);?>
-              <h2><?php if($page_quoted_title){echo nl2br($page_quoted_title);} else {the_title();} ?></h2>
-            </header>
-            <?php the_content();?>
-          </article>
-          <?php $extra_content = get_post_meta($post->ID, 'extra_content', true);?>
-          <?php if($extra_content):?>
-          <aside id="extra_content"><?php echo $extra_content;?></aside>
-          <?php endif;?>
-      <?php endwhile;?>
-      </div>
-    </div>
-
-    <? require '_sponsors.php'?> 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-    <script src="<?= path("bootstrap/js/bootstrap.js") ?>"></script>
-    <script src="<?= path("js/lines.js") ?>"></script>
-  </body>
-</html>
+get_header(); ?>
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<div id='page'>
+  <div class='container'>
+    <article>
+      <header>
+        <?php $page_quoted_title = get_post_meta($post->ID, 'page_quoted_title', true);?>
+        <h2><?php if($page_quoted_title){echo nl2br($page_quoted_title);} else {the_title();} ?></h2>
+      </header>
+      <?php the_content();?>
+    </article>
+    <?php $extra_content = get_post_meta($post->ID, 'extra_content', true);?>
+    <?php if($extra_content):?>
+    <aside id="extra_content"><?php echo $extra_content;?></aside>
+    <?php endif;?>
+<?php endwhile;?>
+<?php get_footer(); ?>
