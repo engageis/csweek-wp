5e83e18ad4d149e49666e7dc41006eedcfd71492
Barbara Wolff Dick <barbara@quavio.com.br>
2012-12-05 10:04:59 -0200

    divided bullets and updated content in home

--- wp-content/themes/csweek/_daily-topics.php
+++ wp-content/themes/csweek/_daily-topics.php
@@ -5,47 +5,48 @@
         <h3>Crowdsourcing Week Asia 2013 to Bring Innovators to Center Stage</h3>
         <h4>Crowdsourcing Week Asia (CSWA) 2013 will bring together the best and brightest minds in the field across disciplines. The 5-day curated event will highlight new directions in crowdsourcing in:</h4>
         <br />
-        <div class="list">
-          <ul>
-            <li>Science/Tech</li>
-            <li>Enterprise</li>
-            <li>Government/NGO</li>
-            <li>Urban Planning</li>
-            <li>Tourism</li>
-            <li>Citizen Journalism</li>
-          </ul>
-        </div>
-        <div class="list">
-          <ul>
-            <li>Crowdfunding & Start-ups</li>
-            <li>Advertising</li>
-            <li>Marketing</li>
-            <li>Design & Fashion</li>
-            <li>Film</li>
-            <li>Photography</li>
-            <li>Open Source... and more.</li>
-          </ul>
-        </div>
+        <ul class="span3">
+          <li>Science/Tech</li>
+          <li>Enterprise</li>
+          <li>Government/NGO</li>
+          <li>Urban Planning</li>
+          <li>Tourism</li>
+        </ul>
+        <ul class="span3">
+          <li>Crowdfunding & Start-ups</li>
+          <li>Advertising</li>
+          <li>Marketing</li>
+          <li>Citizen Journalism</li>
+        </ul>
+        <ul class="span3">
+          <li>Design & Fashion</li>
+          <li>Film</li>
+          <li>Photography</li>
+          <li>Open Source... and more.</li>
+        </ul>
 
-
+    <div class="span12">
+      <div class="row">
         <h3>Who Will Benefit the Most at Crowdsourcing Week?</h3>
-        <ul class="list">
+        <ul class="span3">
           <li>CEO</li>
           <li>CMO</li>
           <li>COO</li>
           <li>CFO</li>
         </ul>
-        <ul class="list">
+        <ul class="span3">
           <li>CCO</li>
           <li>CIO</li>
           <li>Presidents</li>
           <li>Vice-Presidents</li>
         </ul>
-        <ul class="list">
+        <ul class="span3">
           <li>Directors</li>
           <li>Managers</li>
           <li>Partners</li>
         </ul>
+      </div>
+    </div>
 
         <h4>This interactive and engaging global event will be complete with successful crowdsourcing practitioners:</h4>
         <ul class="list">
