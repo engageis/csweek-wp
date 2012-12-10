cf9bd222ae2b2a6e2a9bf02db29411c5e848cc61
Barbara Wolff Dick <barbara@quavio.com.br>
2012-12-10 21:41:36 -0200

    new section for singapore exibt center logo

--- wp-content/themes/csweek/home-page.php
+++ wp-content/themes/csweek/home-page.php
@@ -37,6 +37,18 @@ get_header(); ?>
     </div>
   </div>
 </div>
+
+<div id="support">
+  <div class="container">
+    <div class="row">
+      <div class="offset4 span8">
+        <h2>Supported by</h2>
+        <img src="wp-content/themes/csweek/images/secb.png" id="secb-logo"/>
+      </div>
+    </div>
+  </div>
+</div>
+
 <div id="daily-topics">
   <div class="container">
     <div class="row">
