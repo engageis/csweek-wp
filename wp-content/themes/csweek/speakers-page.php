c08d8aa5396080aff26cef182d0d11aa6ecdf5c6
Barbara Wolff Dick <barbara@quavio.com.br>
2012-12-11 18:28:19 -0200

    linkedin orto mistake

--- wp-content/themes/csweek/speakers-page.php
+++ wp-content/themes/csweek/speakers-page.php
@@ -26,7 +26,7 @@ get_header(); ?>
         <?php $linkedin = get_post_meta($post->ID, 'linked_in', true);?>
         <?php $twitter = get_post_meta($post->ID, 'twitter', true);?>
         <?php if($linkedin):?>
-          <a id="linkedin" href="<?= $linkedin ?>">Linkedin</a>
+          <a id="linkedin" href="<?= $linkedin ?>">Linkedin | </a>
         <?php endif;?>
         <?php if($twitter):?>
           <a id="twitter" href="<?= $twitter ?>">Twitter</a>
