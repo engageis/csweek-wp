0d8f1bca5fc1a56fc0a44ea3556d24855f2272c3
Gustavo Guichard <gustavoguichard@gmail.com>
2012-12-12 16:47:46 -0200

    Link to home

--- wp-content/themes/csweek/_splash-sec.php
+++ wp-content/themes/csweek/_splash-sec.php
@@ -14,7 +14,7 @@
     </script> -->
     
     <div class="row">
-      <a href="<?php echo get_settings('home'); ?>">
+      <a href="<?php bloginfo('url'); ?>">
         <img class="span2" src="<?php bloginfo('template_url');?>/images/logo.png" />
       </a>
 
