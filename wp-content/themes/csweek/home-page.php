5739fec8e829741d392a3cfc04160b4cea62f119
Barbara Wolff Dick <barbara@quavio.com.br>
2012-12-12 23:46:27 -0200

    facebook face thing done

--- wp-content/themes/csweek/home-page.php
+++ wp-content/themes/csweek/home-page.php
@@ -17,21 +17,11 @@ get_header(); ?>
         <?php the_content(); ?>
       </div>
       <div class="offset1 span3">
-        <!-- <div class="fb-like" data-href="http://www.facebook.com/CrowdsourcingWeek?fref=ts" data-send="false" data-width="250" data-show-faces="true"></div> -->
-
-        <div id="open-registration">
-          <!--<div class="pad">
-            <div class="inner">
-              <h2>
-                <span>save the</span>
-                <span class="open">date</span>-->
-          <div class="my-cal btn-save-the-date">
-            <? require '_cal.php' ?>
-          </div>
-            <!--  </h2>
-            </div>
-          </div>-->
-        <div style="width: 250px; float: right;" class="fb-like-box" data-href="http://www.facebook.com/CrowdsourcingWeek/" data-width="250" data-show-faces="true" data-stream="false" data-header="true"></div>
+        <div class="my-cal btn-save-the-date">
+          <? require '_cal.php' ?>
+        </div >
+        <div class="fb-like">
+          <div style="width: 250px; float: right;" class="fb-like-box" data-href="http://www.facebook.com/CrowdsourcingWeek/" data-width="250" data-show-faces="true" data-stream="false" data-header="true"></div>
         </div>
 
         /div<?php endwhile;?>
