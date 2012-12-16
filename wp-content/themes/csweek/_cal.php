45d8de456f37d6882998e8bb6faf296f30a5de62
Juan Maiz <juanmaiz@gmail.com>
2012-12-16 15:29:49 -0200

    yahoo link

--- wp-content/themes/csweek/_cal.php
+++ wp-content/themes/csweek/_cal.php
@@ -33,9 +33,9 @@
   <div class="calendarLine">
     <a target="_blank" href="https://www.google.com/calendar/render?action=TEMPLATE&text=Crowdsourcing%20Week%20-%20Singapore%202013&dates=20130603T110000Z/20130607T213000Z&details=For+details,+link+here:+www.crowdsourcingweek.com&location=81%20Victoria%20Street%20Singapore%20188065%20Google%20Map:%20http://goo.gl/maps/zvJdC&pli=1&sf=true&output=xml"><span class="cal-icon add_to_google">&nbsp;</span> Google Calendar</a>
   </div>
-<!--   <div>
-    <a target="_blank" href="javascript: AddToCalendar('yahoo', 'http://www.eventbrite.com', '4825411937', '');"><span class="cal-icon add_to_yahoo">&nbsp;</span> Yahoo! Calendar</a>
-  </div> -->
+  <div class="calendarLine">
+    <a target="_blank" href="http://calendar.yahoo.com/?v=60&view=d&type=20&title=Crowdsourcing%20Week%20-%20Singapore%202013&st=20130603T110000Z/20130607T213000Z&dur=0115&desc=For+details,+link+here:+www.crowdsourcingweek.com&in_loc=81%20Victoria%20Street%20Singapore%20188065%20Google%20Map:%20http://goo.gl/maps/zvJdC&url=&rpat="><span class="cal-icon add_to_yahoo">&nbsp;</span> Yahoo! Calendar</a>
+  </div>
   <div class="calendarLine">
    <a target="_blank" href="crowdsourcing-week.ics"><span class="cal-icon add_to_ical">&nbsp;</span> iCal Calendar</a>
   </div>
