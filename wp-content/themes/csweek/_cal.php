330f6476ff78607e600ed9ca9ae352c80e0401e4
Juan Maiz <juanmaiz@gmail.com>
2012-12-16 20:09:04 -0200

    correct dates on gmail and ics

--- wp-content/themes/csweek/_cal.php
+++ wp-content/themes/csweek/_cal.php
@@ -31,10 +31,10 @@
     <a target="_blank" href="crowdsourcing-week.ics"><span class="cal-icon add_to_outlook">&nbsp;</span> Outlook Calendar</a>
   </div>
   <div class="calendarLine">
-    <a target="_blank" href="https://www.google.com/calendar/render?action=TEMPLATE&text=Crowdsourcing%20Week%20-%20Singapore%202013&dates=20130603T110000Z/20130607T213000Z&details=For+details,+link+here:+www.crowdsourcingweek.com&location=81%20Victoria%20Street%20Singapore%20188065%20Google%20Map:%20http://goo.gl/maps/zvJdC&pli=1&sf=true&output=xml"><span class="cal-icon add_to_google">&nbsp;</span> Google Calendar</a>
+    <a target="_blank" href="https://www.google.com/calendar/render?action=TEMPLATE&text=Crowdsourcing%20Week%20-%20Singapore%202013&dates=20130603T010000Z/20130607T093000Z&details=For+details,+link+here:+www.crowdsourcingweek.com&location=81%20Victoria%20Street%20Singapore%20188065%20Google%20Map:%20http://goo.gl/maps/zvJdC&pli=1&sf=true&output=xml"><span class="cal-icon add_to_google">&nbsp;</span> Google Calendar</a>
   </div>
   <div class="calendarLine">
-    <a target="_blank" href="http://calendar.yahoo.com/?v=60&view=d&type=20&title=Crowdsourcing%20Week%20-%20Singapore%202013&st=20130603T110000Z/20130607T213000Z&dur=0115&desc=For+details,+link+here:+www.crowdsourcingweek.com&in_loc=81%20Victoria%20Street%20Singapore%20188065%20Google%20Map:%20http://goo.gl/maps/zvJdC&url=&rpat="><span class="cal-icon add_to_yahoo">&nbsp;</span> Yahoo! Calendar</a>
+    <a target="_blank" href="http://calendar.yahoo.com/?v=60&view=d&type=20&title=Crowdsourcing%20Week%20-%20Singapore%202013&st=20130603T010000Z/20130607T093000Z&dur=0115&desc=For+details,+link+here:+www.crowdsourcingweek.com&in_loc=81%20Victoria%20Street%20Singapore%20188065%20Google%20Map:%20http://goo.gl/maps/zvJdC&url=&rpat="><span class="cal-icon add_to_yahoo">&nbsp;</span> Yahoo! Calendar</a>
   </div>
   <div class="calendarLine">
    <a target="_blank" href="crowdsourcing-week.ics"><span class="cal-icon add_to_ical">&nbsp;</span> iCal Calendar</a>
