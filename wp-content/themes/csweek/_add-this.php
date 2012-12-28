271c2bd5d92af67c105a6dbda95c7ae4b6f5c3c8
Barbara Wolff Dick <barbara@quavio.com.br>
2012-12-28 12:15:50 -0200

    new share with bubble icons

--- wp-content/themes/csweek/_add-this.php
+++ wp-content/themes/csweek/_add-this.php
@@ -1,17 +1,17 @@
 <!-- AddThis Button BEGIN -->
 <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-<a class="addthis_button_preferred_1"></a>
-<a class="addthis_button_preferred_2"></a>
-<a class="addthis_button_preferred_3"></a>
-<a class="addthis_button_preferred_4"></a>
-<a class="addthis_button_compact"></a>
+<a class="addthis_button_facebook social-share-margin"></a>
+<a class="addthis_button_twitter social-share-margin"></a>
+<a class="addthis_button_linkedin social-share-margin"></a>
+<a class="addthis_button_google_plusone_badge social-share-margin"></a>
+<a class="addthis_button_compact social-share-margin"></a>
 <a class="addthis_counter addthis_bubble_style"></a>
 </div>
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-50dda7370e0514c4"></script>
+<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50be10b95acf3a88"></script>
 <!-- AddThis Button END -->
 
 
 
-<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-50be10b95acf3a88"></script>
+
 <!-- AddThis Button END -->
