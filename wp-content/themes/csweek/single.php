ee300831401e33b8618c8b8af6afc7c09f44741f
Barbara Wolff Dick <barbara@quavio.com.br>
2012-12-28 17:43:50 -0200

    same structure for authors page and authors bio on blog

--- wp-content/themes/csweek/single.php
+++ wp-content/themes/csweek/single.php
@@ -41,17 +41,19 @@ get_header(); ?>
           <?php echo get_avatar( get_the_author_email(), '80' ); ?>
           <h4><?php the_author_posts_link();?></h4>
           <p><?php echo nl2br(get_the_author_meta('description')); ?></p>
-          <?php $linkedin = get_the_author_meta('linkedin', $post->post_author);?>
-          <?php $twitter = get_the_author_meta('twitter', $post->post_author);?>
-          <?php if($linkedin):?>
-            <a target="_blank" id="linkedin" href="<?= $linkedin ?>">Linkedin </a>
-          <?php endif;?>
-          <?php if($twitter && $linkedin):?>
-             | 
-          <?php endif;?>
-          <?php if($twitter):?>
-            <a target="_blank" id="twitter" href="<?= $twitter ?>"> Twitter</a>
-          <?php endif;?>
+          <div class="author-social">
+            <?php $linkedin = get_the_author_meta('linkedin', $post->post_author);?>
+            <?php $twitter = get_the_author_meta('twitter', $post->post_author);?>
+            <?php if($linkedin):?>
+              <a target="_blank" id="linkedin" href="<?= $linkedin ?>">Linkedin </a>
+            <?php endif;?>
+            <?php if($twitter && $linkedin):?>
+               | 
+            <?php endif;?>
+            <?php if($twitter):?>
+              <a target="_blank" id="twitter" href="<?= $twitter ?>"> Twitter</a>
+            <?php endif;?>
+          </div>
         </footer>
       </div>
     </article>
