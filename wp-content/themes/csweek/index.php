2a01ad4e33132462b6b66fe9d316dabf352b9bc3
Barbara Wolff Dick <barbara@quavio.com.br>
2013-01-05 01:54:25 -0200

    tags for posts

--- wp-content/themes/csweek/index.php
+++ wp-content/themes/csweek/index.php
@@ -25,10 +25,12 @@
           <div class="entry-content">
             <?php if(has_post_thumbnail($post->ID)):?><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail('big-thumb');?></a><?php endif;?>
             <?php the_excerpt(); ?>
-            <section class="tags">
+            <section class="categories">
               <?php $category_list = get_the_category_list( ', ' ); ?>
               <?php if ( $category_list ): ?>
                  <p>Categories: <?=$category_list;?></p>
+            </section>
+            <section class="tags">
               <?php endif; ?>
               <?php $tags_list = get_the_tag_list( '', ', ' ); ?>
               <?php if ( $tags_list ): ?>
