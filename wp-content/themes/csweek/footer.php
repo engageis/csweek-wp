8a28b1a8ddc309cafb2c5b621431ae95a8482f75
Juan Maiz <juanmaiz@gmail.com>
2013-01-06 19:03:11 -0200

    analytics

--- wp-content/themes/csweek/footer.php
+++ wp-content/themes/csweek/footer.php
@@ -44,7 +44,7 @@
               </li>
 
               <li>
-                <a target="_blank" href='http://zooppa.com/corporate/overview '>
+                <a target="_blank" href='http://www.zooppa.com'>
                   <div class="sponsor-logo align-logo">
                     <img src="<?php bloginfo('template_url');?>/images/zooppa.png">
                   </div>
@@ -235,6 +235,16 @@
     <script src="<?php bloginfo('template_url');?>/bootstrap/js/bootstrap.js"></script>
     <script src="<?php bloginfo('template_url');?>/js/lines.js"></script>
     <?php require '_contact.php'; ?> 
+    <script>
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-29337904-1']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
     <?php wp_footer(); ?> 
   </body>
 </html>
