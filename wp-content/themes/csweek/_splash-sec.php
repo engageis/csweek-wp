634397a83fb83ae935da2be19ae93dd9d583cd72
Gustavo Guichard <gustavoguichard@gmail.com>
2013-01-28 12:52:53 -0200

    Adjusting splash_sec

--- wp-content/themes/csweek/_splash-sec.php
+++ wp-content/themes/csweek/_splash-sec.php
@@ -2,44 +2,18 @@
 ================================================== -->
 <header class="jumbotron subhead" id="overview">
   <div class="container">
-    <!--canvas id="nodes" height="180px" width="1200px" style="z-index:-1">
-    </canvas>
-    <script type="text/javascript">
-      resize_canvas = function(e){
-        var canvas = document.getElementsByTagName('canvas')[0];
-        canvas.width  = window.innerWidth - 200;
-      }
-      window.onresize = resize_canvas
-      window.onload = resize_canvas
-    </script> -->
-    
-    <div class="row">
-      <a href="<?php bloginfo('url'); ?>">
-        <img class="span2" src="<?php bloginfo('template_url');?>/images/logo.png" />
-      </a>
-
-      <div class="small-lettering span4 offset2">
-        <div class="row-fluid">
-          <div class="span12">
-            <h1>
-              <span class="days">03-07</span>
-              <span class="month">JUNE</span>
-            </h1>
-          </div>
-        </div>
-        <div class="row-fluid">
-          <div class="span12">
-            <h1>
-              <span class="year">2013</span>
-              <span class="where">SINGAPORE</span>
-            </h1>
-          </div>
-        </div>
-      </div>
-      <div class="save-the-date span4">
-        <? //require 'save-the-date.php' ?>
-      </div>
+    <a href="<?php bloginfo('url'); ?>">
+      <img class="span2" src="<?php bloginfo('template_url');?>/images/logo.png" />
+    </a>
+    <div class="small-lettering">
+      <h1>
+        <span class="days">03-07</span>
+        <span class="month">JUNE</span>
+      </h1>
+      <h1>
+        <span class="year">2013</span>
+        <span class="where">SINGAPORE</span>
+      </h1>
     </div>
-    
   </div>
 </header>
