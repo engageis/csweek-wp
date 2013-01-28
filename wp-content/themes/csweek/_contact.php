3bae2eef59eda653b17c157e8099bd0cb4d043b5
Gustavo Guichard <gustavoguichard@gmail.com>
2013-01-28 13:34:35 -0200

    Trying to fix footetr

--- wp-content/themes/csweek/_contact.php
+++ wp-content/themes/csweek/_contact.php
@@ -1,23 +1,6 @@
 <div id="contact">
   <div class="container">
     <div class="row">
-      <!--div class="span7">
-        <h2> Blog Feed </h2>
-        <style>
-          #blog p{color: #ccc;}
-        </style>
-        <div id="blog">
-          <?php $my_query = new WP_Query('showposts=5&post_type=post&order=DESC'); ?>  
-          <?php if($my_query->have_posts()) : ?><?php while($my_query->have_posts()) : $my_query->the_post(); ?>
-            <div class="post">
-              <small style="color:#666;margin:0;line-height:0"><?php the_date(); ?></small>
-              <br />
-              <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-              <?php the_excerpt(); ?>
-            </div>
-          <?php endwhile; endif; wp_reset_query(); ?>
-        </div>
-      </div-->
       <div class="span5">
         <h2 id="contact-info">Contact us</h2>
         <h3>Send us a <a href="<?= get_permalink_by_name("contact"); ?>">message</a></h3>
--- wp-content/themes/csweek/footer.php
+++ wp-content/themes/csweek/footer.php
@@ -1,4 +1,3 @@
-
     <div id="sponsors">
       <div class="container">
         <div class="row">
@@ -6,9 +5,8 @@
             <a target="_blank" href="http://www.yoursingapore.com/content/traveller/en/experience.html"><img src="<?php bloginfo('template_url');?>/images/ysing.jpg" class="ysing-logo"/></a>
           </div>
         </div>
-        <div class="row"> 
+        <div class="row">
           <div class="span12"u>
-            
 
 <!-- DESCOMENTAR ESSAS LINHAS QUANDO ESTIVER TUDO OK
             <?php $my_query = new WP_Query('showposts=-1&genre=crowd-sponsors&post_type=sponsors');?>
@@ -54,7 +52,6 @@
             <?php endif; wp_reset_query(); ?>
  -->
 
-
             <h2>Crowdsourcing Sponsors <a href="<?= get_permalink_by_name("sponsors"); ?>" class="learn-more">(learn more)</a></h2>
             <ul class="sponsors-list">
               <li>
@@ -64,7 +61,6 @@
                   </div>
                 </a>
               </li>
-
               <li>
                 <a target="_blank" href='http://www.razoo.com/'>
                   <div class="sponsor-log align-logo">
@@ -72,7 +68,6 @@
                   </div>
                 </a>
               </li>
-
               <li>
                 <a target="_blank" href='http://www.imagebrief.com/'>
                   <div class="sponsor-logo">
@@ -80,7 +75,6 @@
                   </div>
                 </a>
               </li>
-
               <li>
                 <a target="_blank" href='http://www.chaordix.com/'>
                   <div class="sponsor-logo">
@@ -88,7 +82,6 @@
                   </div>
                 </a>
               </li>
-
               <li>
                 <a target="_blank" href='http://www.zooppa.com'>
                   <div class="sponsor-logo align-logo">
@@ -103,7 +96,6 @@
                   </div>
                 </a>
               </li>
-              
               <li>
                 <a target="_blank" href='http://en.eyeka.com/'>
                   <div class="sponsor-logo align-logo">
@@ -112,10 +104,11 @@
                 </a>
               </li>
             </ul>
+          </div>
+        </div>
         <div class="row">
           <div class= "span12">
             <ul class="sponsors-list">
-
               <li>
                 <a target="_blank" href='http://www.clozette.co'>
                   <div class="sponsor-logo">
@@ -123,7 +116,6 @@
                   </div>
                 </a>
               </li>
-              
               <li>
                 <a target="_blank" href='http://www.expressinmusic.com/home/default.aspx'>
                   <div class="sponsor-logo">
@@ -131,7 +123,6 @@
                   </div>
                 </a>
               </li>
-
               <li>
                 <a target="_blank" href='http://www.growvc.com/main/'>
                   <div class="sponsor-logo">
@@ -139,7 +130,6 @@
                   </div>
                 </a>
               </li>
-
               <li>
                 <a target="_blank" href='http://www.frogdesign.com'>
                   <div class="sponsor-logo invert-align">
@@ -147,7 +137,6 @@
                   </div>
                 </a>
               </li>
-
               <li>
                 <a target="_blank" href='http://www.talenthouse.com'>
                   <div class="sponsor-logo make-it-bigger">
@@ -155,109 +144,88 @@
                   </div>
                 </a>
               </li>
-
-
             </ul>
           </div>
         </div>
-
-
-        </div>  
         <div class="span12">
           <h2>Technology & strategic partners</h2>
-            <ul class="sponsors-list">
-              <li>
-                <a target="_blank" href='http://www.powertothecrowd.com/'>
-                  <div class="sponsor-logo">
-                    <img src="<?php bloginfo('template_url');?>/images/engage.png">
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a target="_blank" href='http://www.appbackr.com/'>
-                  <div class="sponsor-logo align-logo">
-                    <img src="<?php bloginfo('template_url');?>/images/appbackr.png">
-                  </div>
-                </a>
-              </li>
-
-              <li>
-                <a target="_blank" href='http://www.bnotions.com/'>
-                  <div class="sponsor-logo align-logo">
-                    <img src="<?php bloginfo('template_url');?>/images/bnotions.png">
-                  </div>
-                </a>
-              </li>
-
-              <li>
-                <a target="_blank" href='http://www.yoose.com/'>
-                  <div class="sponsor-logo align-logo">
-                    <img src="<?php bloginfo('template_url');?>/images/yoose.png">
-                  </div>
-                </a>
-              </li>
-
-              <li>
-                <a target="_blank" href='http://www.iminds.be/en'>
-                  <div class="sponsor-logo">
-                    <img src="<?php bloginfo('template_url');?>/images/iminds.png">
-                  </div>
-                </a>
-              </li>
-
-              <li>
-                <a target="_blank" href='http://www.internetmedialabs.com/'>
-                  <div class="sponsor-logo make-it-bigger">
-                    <img src="<?php bloginfo('template_url');?>/images/internetmedialabs.png">
-                  </div>
-                </a>
-              </li>
-
-
-            </ul>
+          <ul class="sponsors-list">
+            <li>
+              <a target="_blank" href='http://www.powertothecrowd.com/'>
+                <div class="sponsor-logo">
+                  <img src="<?php bloginfo('template_url');?>/images/engage.png">
+                </div>
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href='http://www.appbackr.com/'>
+                <div class="sponsor-logo align-logo">
+                  <img src="<?php bloginfo('template_url');?>/images/appbackr.png">
+                </div>
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href='http://www.bnotions.com/'>
+                <div class="sponsor-logo align-logo">
+                  <img src="<?php bloginfo('template_url');?>/images/bnotions.png">
+                </div>
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href='http://www.yoose.com/'>
+                <div class="sponsor-logo align-logo">
+                  <img src="<?php bloginfo('template_url');?>/images/yoose.png">
+                </div>
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href='http://www.iminds.be/en'>
+                <div class="sponsor-logo">
+                  <img src="<?php bloginfo('template_url');?>/images/iminds.png">
+                </div>
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href='http://www.internetmedialabs.com/'>
+                <div class="sponsor-logo make-it-bigger">
+                  <img src="<?php bloginfo('template_url');?>/images/internetmedialabs.png">
+                </div>
+              </a>
+            </li>
+          </ul>
         </div>
-
         <div class="span12">
           <h2>Media Partners</h2>
-          <!--<h3>Global Media Partners</h3>
-          <h3>Regional Partners</h3>-->
-          <!--h3>Bloggers</h3-->
-            <ul class="sponsors-list">
-              <li>
-                <a target="_blank" href='http://www.techinasia.com/'>
-                  <div class="sponsor-logo">
-                    <img src="<?php bloginfo('template_url');?>/images/techasia.png">
-                  </div>
-                </a>
-              </li>
-              
-              <li>
-                <a target="_blank" href='http://sgentrepreneurs.com'>
-                  <div class="sponsor-logo">
-                    <img src="<?php bloginfo('template_url');?>/images/sge.png">
-                  </div>
-                </a>
-              </li>
-          
-              <li>
-                <a target="_blank" href='http://www.e27.sg/'>
-                  <div class="sponsor-logo">
-                    <img src="<?php bloginfo('template_url');?>/images/e27.png">
-                  </div>
-                </a>
-              </li>
-
-              <li>
-                <a target="_blank" href='http://www.crowdsourcingblog.de/'>
-                  <div class="sponsor-logo make-it-bigger">
-                    <img src="<?php bloginfo('template_url');?>/images/crowdsourcingblog.png">
-                  </div>
-                </a>
-              </li>
-
-
-            </ul>
-
+          <ul class="sponsors-list">
+            <li>
+              <a target="_blank" href='http://www.techinasia.com/'>
+                <div class="sponsor-logo">
+                  <img src="<?php bloginfo('template_url');?>/images/techasia.png">
+                </div>
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href='http://sgentrepreneurs.com'>
+                <div class="sponsor-logo">
+                  <img src="<?php bloginfo('template_url');?>/images/sge.png">
+                </div>
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href='http://www.e27.sg/'>
+                <div class="sponsor-logo">
+                  <img src="<?php bloginfo('template_url');?>/images/e27.png">
+                </div>
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href='http://www.crowdsourcingblog.de/'>
+                <div class="sponsor-logo make-it-bigger">
+                  <img src="<?php bloginfo('template_url');?>/images/crowdsourcingblog.png">
+                </div>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -265,7 +233,7 @@
     <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
     <script src="<?php bloginfo('template_url');?>/bootstrap/js/bootstrap.js"></script>
     <script src="<?php bloginfo('template_url');?>/js/lines.js"></script>
-    <?php require '_contact.php'; ?> 
+    <?php require '_contact.php'; ?>
     <script>
       var _gaq = _gaq || [];
       _gaq.push(['_setAccount', 'UA-29337904-1']);
@@ -276,6 +244,6 @@
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
       })();
     </script>
-    <?php wp_footer(); ?> 
+    <?php wp_footer(); ?>
   </body>
 </html>
