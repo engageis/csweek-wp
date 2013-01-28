632f638e9a2a71df01a8bc9f5b8a2b46c23df04b
Gustavo Guichard <gustavoguichard@gmail.com>
2013-01-28 12:58:45 -0200

    Fixes contact page

--- wp-content/themes/csweek/contact.php
+++ wp-content/themes/csweek/contact.php
@@ -8,9 +8,9 @@
  */
 
 get_header(); ?>
-<div id='page'>
+<div id="page" class="contact-page">
   <div class='container'>
-    <div class="lettering span8 offset4">
+    <div class="lettering">
       <iframe id="wufooFormz7x3x5" height="659" allowtransparency="true" frameborder="0" scrolling="no" style="width:500px;border:none" src="http://csweek.wufoo.com/embed/z7x3x5/def/embedKey=z7x3x5694340&amp;referrer=http%3Awuslashwuslashcrowdsourcingweek.comwuslash">&lt;a href="http://csweek.wufoo.com/forms/z7x3x5/" title="html form"&gt;Fill out my Wufoo form!&lt;/a&gt;</iframe>
     </div>
   </div>
