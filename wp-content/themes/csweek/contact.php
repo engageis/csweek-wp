54f8351ee9222e6acc5eaf9405e7e0c7597d6aed
Gustavo Guichard <gustavoguichard@gmail.com>
2013-01-28 12:56:26 -0200

    Fixes contact page and splash_sec

--- wp-content/themes/csweek/contact.php
+++ wp-content/themes/csweek/contact.php
@@ -8,9 +8,11 @@
  */
 
 get_header(); ?>
-<div class="row">
-  <div class="lettering span8 offset4">
-    <iframe id="wufooFormz7x3x5" height="659" allowtransparency="true" frameborder="0" scrolling="no" style="width:500px;border:none" src="http://csweek.wufoo.com/embed/z7x3x5/def/embedKey=z7x3x5694340&amp;referrer=http%3Awuslashwuslashcrowdsourcingweek.comwuslash">&lt;a href="http://csweek.wufoo.com/forms/z7x3x5/" title="html form"&gt;Fill out my Wufoo form!&lt;/a&gt;</iframe>
+<div id='page'>
+  <div class='container'>
+    <div class="lettering span8 offset4">
+      <iframe id="wufooFormz7x3x5" height="659" allowtransparency="true" frameborder="0" scrolling="no" style="width:500px;border:none" src="http://csweek.wufoo.com/embed/z7x3x5/def/embedKey=z7x3x5694340&amp;referrer=http%3Awuslashwuslashcrowdsourcingweek.comwuslash">&lt;a href="http://csweek.wufoo.com/forms/z7x3x5/" title="html form"&gt;Fill out my Wufoo form!&lt;/a&gt;</iframe>
+    </div>
   </div>
 </div>
 <?php get_footer(); ?>
