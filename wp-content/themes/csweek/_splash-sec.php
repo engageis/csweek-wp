c636a2039307c5a12ff2025dc0cd515f0ba930f1
Gustavo Guichard <gustavoguichard@gmail.com>
2013-02-14 20:05:58 -0200

    Adds pre-register button in every page

--- wp-content/themes/csweek/_splash-sec.php
+++ wp-content/themes/csweek/_splash-sec.php
@@ -15,5 +15,6 @@
         <span class="where">SINGAPORE</span>
       </h1>
     </div>
+    <a href="<?= get_permalink_by_name("contact"); ?>" class="save-the-date">Pre-register now</a>
   </div>
 </header>
