623354eb8632f516872909ee0aada97686d4a2f9
Gustavo Guichard <gustavoguichard@gmail.com>
2013-04-03 16:04:52 -0300

    Adds Mailchimp form to blog sidebar

--- wp-content/themes/csweek/sidebar.php
+++ wp-content/themes/csweek/sidebar.php
@@ -1,3 +1,21 @@
 <aside id="sidebar" class="the-sidebar">
   <?php dynamic_sidebar('sidebar-primary'); ?>
+  <!-- Begin MailChimp Signup Form -->
+  <link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+  <style type="text/css">
+  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+  /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+  We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+  </style>
+  <div id="mc_embed_signup">
+    <form action="http://crowdsourcingweek.us6.list-manage.com/subscribe/post?u=6b9a863a2a&amp;id=cf655e054d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <label for="mce-EMAIL">Get Crowdsourcing Week news &amp; updates!</label>
+      <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+      <div class="clear">
+        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+      </div>
+    </form>
+  </div>
+
+  <!--End mc_embed_signup-->
 </aside>
