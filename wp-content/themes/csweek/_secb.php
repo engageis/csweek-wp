bfce99e91b56c1bf2340be5cdfb50664aaa372c0
Gustavo Guichard <gustavoguichard@gmail.com>
2013-05-21 11:08:33 -0300

    Added ACE-10 logo

--- wp-content/themes/csweek/_secb.php
+++ wp-content/themes/csweek/_secb.php
@@ -5,6 +5,7 @@
         <!-- <h2>Supported by</h2> -->
         <a href="http://www.yoursingapore.com/content/mice/en/why-singapore/singapore-exhibition-convention-bureau.html"><img src="<?php bloginfo('template_url');?>/images/secb.png" id="secb-logo"/></a>
       </div>
+      <img src="<?php bloginfo('template_url');?>/images/ace10.jpg" class="ace10-logo"/>
       <a target="_blank" href="http://www.yoursingapore.com/content/traveller/en/experience.html"><img src="<?php bloginfo('template_url');?>/images/ysing.jpg" class="ysing-logo"/></a>
     </div>
   </div>
