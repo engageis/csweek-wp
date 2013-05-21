b7db5487a913c96c7c97824664fdf8407eb3ad30
Gustavo Guichard <gustavoguichard@gmail.com>
2013-05-21 10:43:27 -0300

    Fixing a logo

--- wp-content/themes/csweek/_secb.php
+++ wp-content/themes/csweek/_secb.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="offset4 span8">
         <h2>Supported by</h2>
-        <img src="wp-content/themes/csweek/images/secb.png" id="secb-logo"/>
+        <a href="http://www.yoursingapore.com/content/mice/en/why-singapore/singapore-exhibition-convention-bureau.html"><img src="<?php bloginfo('template_url');?>/images/secb.png" id="secb-logo"/></a>
       </div>
       <a target="_blank" href="http://www.yoursingapore.com/content/traveller/en/experience.html"><img src="<?php bloginfo('template_url');?>/images/ysing.jpg" class="ysing-logo"/></a>
     </div>
