cda5a6390b8251b948c0afa3547054b0200341c4
Gustavo Guichard <gustavoguichard@gmail.com>
2013-05-22 09:56:35 -0300

    Logos modifications

--- wp-content/themes/csweek/footer.php
+++ wp-content/themes/csweek/footer.php
@@ -33,7 +33,7 @@
               </div>
               <div class="last-logos">
                 <h2>OFFICIAL AIRLINE NETWORK</h2>
-                <a target="_blank" href="http://www.staralliance.com/"><img src="<?php bloginfo('template_url');?>/images/staralliance.png"/></a>
+                <a target="_blank" href="http://www.staralliance.com/en/business-solutions/conventions-plus/delegates/"><img src="<?php bloginfo('template_url');?>/images/staralliance.png"/></a>
               </div>
             </section>
           </div>
