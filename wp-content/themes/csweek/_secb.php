150c53cdaf8325c19936252ebf563165c3d41f0c
Gustavo Guichard <gustavoguichard@gmail.com>
2013-05-27 10:44:38 -0300

    Updates title of premium partners

--- wp-content/themes/csweek/_secb.php
+++ wp-content/themes/csweek/_secb.php
@@ -1,10 +1,15 @@
 <div id="support">
   <div class="container">
     <div class="row">
-      <h2>Supported by</h2>
-      <a target="_blank" href="http://www.microsoft.com/"><img src="<?php bloginfo('template_url');?>/images/microsoft.jpg" id="microsoft-logo"/></a>
-      <a target="_blank" href="http://www.yoursingapore.com/content/mice/en/why-singapore/singapore-exhibition-convention-bureau.html"><img src="<?php bloginfo('template_url');?>/images/secb.png" id="secb-logo"/></a>
-      <a target="_blank" href="http://ace.sg/"><img src="<?php bloginfo('template_url');?>/images/ace10.jpg" class="ace10-logo"/></a>
+      <div id="premium-logos">
+        <h2>Premium Partner</h2>
+        <a target="_blank" href="http://www.microsoft.com/"><img src="<?php bloginfo('template_url');?>/images/microsoft.jpg" id="microsoft-logo"/></a>
+      </div>
+      <div id="supported-logos">
+        <h2>Supported by</h2>
+        <a target="_blank" href="http://www.yoursingapore.com/content/mice/en/why-singapore/singapore-exhibition-convention-bureau.html"><img src="<?php bloginfo('template_url');?>/images/secb.png" id="secb-logo"/></a>
+        <a target="_blank" href="http://ace.sg/"><img src="<?php bloginfo('template_url');?>/images/ace10.jpg" class="ace10-logo"/></a>
+      </div>
       <a target="_blank" href="http://www.yoursingapore.com/content/traveller/en/experience.html"><img src="<?php bloginfo('template_url');?>/images/ysing.jpg" class="ysing-logo"/></a>
     </div>
   </div>
